9abb66c179f536b033ddcb6e620c6f497b95d5c5
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-03-12 23:32:24 +0100

    ajout CDN Bootstrap

--- index.php
+++ index.php
@@ -3,12 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>Garage Vincent Parrot</title>
 </head>
 <body>
-    <h1>Bonjour et bienvenue sur notre page</h1>
+    <h1>Hello Vincent Parrot !</h1>
 
-<script src="/script.js"></script>
+
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="./assets/img/homepage.jpg" class="d-block mx-lg-auto img-fluid"
+         alt="Photo garage vincent Parrot" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Garage Vincent Parrot,
+            votre expert en réparation automobile</h1>
+        <p class="lead">Quickly design and customize responsive mobile-first sites with
+            Bootstrap, the world’s most popular front-end open source toolkit, featuring Sas
+             variables and mixins, responsive grid system, extensive prebuilt components,
+              and powerful JavaScript plugins.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Voir nos véhicule d'Occasion</button>
+        </div>
+      </div>
+    </div>
+  
+
+<script src="./assets/js/script.js"></script>
 </body>
 </html>
