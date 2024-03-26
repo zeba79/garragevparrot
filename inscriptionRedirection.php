f7b5c60ff9e8ca3fd8a87a176788e7f419f859af
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-03-26 14:39:22 +0100

    Création page de redirection après inscription

--- inscriptionRedirection.php
+++ inscriptionRedirection.php
@@ -1,11 +1,16 @@
-<?php
-require_once '/templates/header.php';
-?>
-<h4 class="alert alert-danger mx-5 text-center">
-    <?php echo "Insertion réussie dans la base de données" ?>
-</h4>
-<a href="index.php" class="btn parrotbtn mx-3 px-3">Retour à la page d'acceuil</a>
 
 <?php
-require_once __DIR__ . '/templates/footer.php';
+require_once './templates/header.php';
+?>
+<h4 class="alert alert-success mx-auto text-center">
+    <?php echo "Nous avons bien enregistré vos informations dans notre base de données.
+     Nous vous remercions pour votre inscription. Découvrez nos services en cliquant sur
+     le lien ci-dessous, à très bientôt sur notre page pour profiter de nos services. " ?>
+</h4>
+<div class="row">
+    <a href="index.php" class="btn parrotbtn  mb-4">Retour à la page d'acceuil</a>
+</div>
+
+<?php
+require_once './templates/footer.php';
 ?>
